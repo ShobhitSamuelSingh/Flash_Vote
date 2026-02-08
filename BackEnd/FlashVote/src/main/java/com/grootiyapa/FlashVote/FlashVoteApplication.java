2fb9d3881f197b9ad188f6bf799a617e3ad1bb9f
Shobhit <shobhitss2225@gmail.com>
2026-02-08 16:29:40 +0530

    Add : Folder structure

---
+++ BackEnd/FlashVote/src/main/java/com/grootiyapa/FlashVote/FlashVoteApplication.java
@@ -0,0 +1,13 @@
+package com.grootiyapa.FlashVote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashVoteApplication.class, args);
+	}
+
+}
---
+++ BackEnd/FlashVote/src/test/java/com/grootiyapa/FlashVote/FlashVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grootiyapa.FlashVote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
